78297e5c75639930ea721c2daf56ceb5aff2aa93
Andy G <andy.giannini@wellspringworldwide.com>
2014-10-19 15:41:26 -0500

    Adding code from Set 1

---
+++ xor.rb
@@ -0,0 +1,11 @@
+
+def hex_to_base64(hex_string)
+    [[hex_string].pack('H*')].pack('m0')
+end
+
+def xor(h1, h2)
+    (h1.to_i(16) ^ h2.to_i(16)).to_s(16)
+end
+
+#print xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')
+print xor(ARGV[0], ARGV[1])
